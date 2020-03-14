8030d36d61ea7e95192fe986ad5fdd61e872f038
Supasin S <supasinet@gmail.com>
2020-03-14 15:18:55 +0700

    add Service Provider

---
+++ src/ItemServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Nelisys\LaravelPackage;
+
+use Illuminate\Support\ServiceProvider;
+
+class ItemServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        //
+    }
+
+    public function register()
+    {
+        //
+    }
+}
--- tests/ItemTest.php
+++ tests/ItemTest.php
@@ -4,9 +4,17 @@ namespace Nelisys\LaravelPackage\Tests;
 
 use Orchestra\Testbench\TestCase;
 use Nelisys\LaravelPackage\Item;
+use Nelisys\LaravelPackage\ItemServiceProvider;
 
 class ItemTest extends TestCase
 {
+    protected function getPackageProviders($app)
+    {
+        return [
+            ItemServiceProvider::class,
+        ];
+    }
+
     /** @test */
     public function it_can_say_hello()
     {
